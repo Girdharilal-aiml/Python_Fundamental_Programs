ed0cd6f42888183990a5bca4c2d994e20c8e6438
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-04 15:19:09 +0500

    Create 3 Type conversion (int, float, str).py

---
+++ 3 Type conversion (int, float, str).py
@@ -0,0 +1,8 @@
+def type_conversion():
+    num_str = "123"
+    num_int = int(num_str)
+    num_float = float(num_str)
+    
+    print(f"String: {num_str}, Type: {type(num_str)}")
+    print(f"Integer: {num_int}, Type: {type(num_int)}")
+    print(f"Float: {num_float}, Type: {type(num_float)}")
