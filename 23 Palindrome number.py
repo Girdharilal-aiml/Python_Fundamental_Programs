237f75aca7c62c9e5ae367b0766554b79060b1f8
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-04 15:19:00 +0500

    Create 23 Palindrome number.py

---
+++ 23 Palindrome number.py
@@ -0,0 +1,14 @@
+def check_palindrome_number():
+    num = int(input("Enter a number: "))
+    original = num
+    reversed_num = 0
+    
+    while num > 0:
+        digit = num % 10
+        reversed_num = reversed_num * 10 + digit
+        num //= 10
+    
+    if original == reversed_num:
+        print(f"{original} is a Palindrome")
+    else:
+        print(f"{original} is not a Palindrome")
