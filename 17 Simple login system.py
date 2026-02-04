f6ee35bd8fe2ac1b78cd869c18ae3f0f7ac86489
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-04 15:18:41 +0500

    Create 17 Simple login system.py

---
+++ 17 Simple login system.py
@@ -0,0 +1,11 @@
+def simple_login():
+    correct_username = "admin"
+    correct_password = "pass123"
+    
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    
+    if username == correct_username and password == correct_password:
+        print("Login Successful!")
+    else:
+        print("Invalid credentials!")
