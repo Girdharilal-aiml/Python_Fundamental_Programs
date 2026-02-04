37ee151c49d3d90d2567a7234cc65c676fd33485
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-04 15:18:32 +0500

    Create 14 Largest of 3 numbers.py

---
+++ 14 Largest of 3 numbers.py
@@ -0,0 +1,7 @@
+def largest_of_three():
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    c = float(input("Enter third number: "))
+    
+    largest = max(a, b, c)
+    print(f"Largest: {largest}")
