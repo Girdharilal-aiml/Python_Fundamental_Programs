35d00d1d2b1bd264ef44f9e0d4c30d7dfb1bc90d
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-05 14:05:14 +0500

    Create 48 Merge sort.py

---
+++ 48 Merge sort.py
@@ -0,0 +1,30 @@
+def merge_sort_demo():
+    def merge_sort(arr):
+        if len(arr) <= 1:
+            return arr
+        
+        mid = len(arr) // 2
+        left = merge_sort(arr[:mid])
+        right = merge_sort(arr[mid:])
+        
+        return merge(left, right)
+    
+    def merge(left, right):
+        result = []
+        i = j = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
+    
+    lst = [int(x) for x in input("Enter numbers: ").split()]
+    sorted_lst = merge_sort(lst)
+    print(f"Sorted list: {sorted_lst}")
