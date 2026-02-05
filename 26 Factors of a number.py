2ae753de659e6ee9fc5939d7b3284b589a13f88b
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-05 14:04:03 +0500

    Create 26 Factors of a number.py

---
+++ 26 Factors of a number.py
@@ -0,0 +1,8 @@
+def find_factors():
+    num = int(input("Enter a number: "))
+    print(f"Factors of {num}: ", end="")
+    
+    for i in range(1, num + 1):
+        if num % i == 0:
+            print(i, end=" ")
+    print()
