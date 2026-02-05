595f6027ba1ef4cdd9d64d79ac03c9b0d254b20e
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-05 14:05:08 +0500

    Create 46 Insertion sort.py

---
+++ 46 Insertion sort.py
@@ -0,0 +1,12 @@
+def insertion_sort():
+    lst = [int(x) for x in input("Enter numbers: ").split()]
+    
+    for i in range(1, len(lst)):
+        key = lst[i]
+        j = i - 1
+        while j >= 0 and lst[j] > key:
+            lst[j + 1] = lst[j]
+            j -= 1
+        lst[j + 1] = key
+    
+    print(f"Sorted list: {lst}")
