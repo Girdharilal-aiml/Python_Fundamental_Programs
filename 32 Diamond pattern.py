8b8aea3035bffca4ba09955f3d743ee2618dfc53
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-05 14:04:26 +0500

    Create 32 Diamond pattern.py

---
+++ 32 Diamond pattern.py
@@ -0,0 +1,10 @@
+def diamond_pattern():
+    n = int(input("Enter number of rows: "))
+    
+    # Upper half
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "* " * i)
+    
+    # Lower half
+    for i in range(n - 1, 0, -1):
+        print(" " * (n - i) + "* " * i)
