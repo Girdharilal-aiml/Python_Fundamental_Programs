b959620846ffd2e260faa3ea7a223170f3abdd95
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-06 17:18:52 +0500

    Create 55 Replace characters.py

---
+++ 55 Replace characters.py
@@ -0,0 +1,5 @@
+def replace_characters():
+    s = input("Enter a string: ")
+    old_char = input("Character to replace: ")
+    new_char = input("Replace with: ")
+    print(f"Result: {s.replace(old_char, new_char)}")
