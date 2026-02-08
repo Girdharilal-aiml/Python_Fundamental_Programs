3d6f408883938d6deeaece4e133994b6ded61678
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-08 21:37:01 +0500

    Create 78 Try–except.py

---
+++ 78 Try–except.py
@@ -0,0 +1,6 @@
+def basic_exception():
+    try:
+        num = int(input("Enter a number: "))
+        print(f"You entered: {num}")
+    except ValueError:
+        print("Invalid input! Please enter a number.")
