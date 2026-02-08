1b521ae8f142986aad068d84a9543431140bfea0
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-08 21:37:13 +0500

    Create 82 Constructor.py

---
+++ 82 Constructor.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name, roll_no, marks):
+        self.name = name
+        self.roll_no = roll_no
+        self.marks = marks
+    
+    def display(self):
+        print(f"Name: {self.name}, Roll: {self.roll_no}, Marks: {self.marks}")
